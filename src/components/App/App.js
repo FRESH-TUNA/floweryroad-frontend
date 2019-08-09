f047bb00e27e77e22baef1ea45fe9fbeb66ddd1f
DONGWON KIM <lunacircle4@gmail.com>
2019-08-09 20:34:44 +0900

    refactor: change variable name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,19 +3,18 @@ import '../../css/components/App.css';
 import { Route, Switch } from 'react-router-dom'
 
 import Home from '../../routes/home/home'
-import MeetupList from '../../routes/searchResult/searchFlower'
+import SearchResult from '../../routes/searchResult/searchFlower'
 import Signin from '../../routes/signin'
 import Signup from '../../routes/signup'
-import SearchHeader from '../../components/common/searchHeader'
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/meetup' component={() => <MeetupList testprops={'a'} />} />  
+        <Route path='/search' component={() => <SearchResult testprops={'a'} />} />  
         <Route path='/signin' component={() => <Signin testprops={'a'} />} />  
         <Route path='/signup' component={() => <Signup testprops={'a'} />} />  
-        <Route path='/test' component={() => <SearchHeader testprops={'a'} />} />
         <Route path='/' component={() => <Home testprops={'a'} />} />
       </Switch>
     </div>
--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -53,9 +53,9 @@ class Home extends React.Component {
                 <h1>풀꽃길</h1>
                 <div className="search-bar">
                     <input type="text" placeholder="족보 검색하기" />
-                    <button type="button" onClick={() => this.props.history.push('/meetup')}><img src="https://image.flaticon.com/icons/svg/149/149852.svg" /></button>
+                    <button type="button" onClick={() => this.props.history.push('/search')}><img src="https://image.flaticon.com/icons/svg/149/149852.svg" /></button>
                 </div>
-                <button type="button" onClick={() => this.props.history.push('/meetup')}>화제의 꽃</button>
+                <button type="button" onClick={() => this.props.history.push('/recommend')}>화제의 꽃</button>
                 <div className="slider">
                     <Slider {...settings}>
                         <div>
