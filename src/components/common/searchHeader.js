de662ee4a4774cc86634d0eec106c0dc75b9fd28
DONGWON KIM <lunacircle4@gmail.com>
2019-08-12 11:03:40 +0900

    fix: safari competable and modify input placeholder

--- src/components/common/searchHeader.js
+++ src/components/common/searchHeader.js
@@ -14,7 +14,7 @@ class SearchHeader extends React.Component {
             <div className="searchHeader">
                 <h3 onClick={() => this.props.history.push('/')}>풀꽃길</h3>
                 <div className="search-bar">
-                    <input type="text" placeholder="꽃 검색하기" onChange={(event) => this.setState({search: event.target.value})}/>
+                    <input type="text" placeholder="이름, 꽃말, 목적, 색상 으로 검색하기" onChange={(event) => this.setState({search: event.target.value})}/>
                     <button type="button" onClick={() => this.props.history.push('/search?search=' + this.state.search)}><img src="https://image.flaticon.com/icons/svg/149/149852.svg" /></button>
                 </div>
                 <button className="auth"></button>
