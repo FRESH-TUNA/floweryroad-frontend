15fe797cef660fbdae69c708d407c67ed3a5d756
DONGWON KIM <lunacircle4@gmail.com>
2019-08-16 00:15:08 +0900

    feat implement comment auto loading

--- src/routes/flowerDetail/flowerDetail.js
+++ src/routes/flowerDetail/flowerDetail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import  '../../css/routes/flowerDetail.css'
+import '../../css/routes/flowerDetail.css'
 
 import { withRouter, Link } from "react-router-dom";
 import { connect } from 'react-redux';
@@ -21,78 +21,88 @@ class FlowerDetail extends React.Component {
         this.state = {
             isLoading: true,
             flower: {},
-            newCommentState: false
+            newCommentState: false,
+            commentsCount: 6,
+            lastCommentPage: 1
         }
         this.openNewComment = this.openNewComment.bind(this)
         this.closeNewComment = this.closeNewComment.bind(this)
         this.readComments = this.readComments.bind(this)
+        this.checkComments = this.checkComments.bind(this)
         this.newComment = this.newComment.bind(this)
     }
 
     componentDidMount() {
-        console.log(this.props)
         axios('/flowers/' + this.props.match.params.id)
             .then(response => this.setState({ flower: response.data }))
-            .then(() => {return this.readComments()})
-            .then(response => {this.setState({comments: response.data.comments, isLoading: false});})
+            .then(() => { return this.readComments(1) })
+            .then(response => { this.setState({ comments: response.data.comments, isLoading: false }); })
     }
 
-    readComments() {
-        return axios(
-            '/flowers/' + this.props.match.params.id + '/comments')
+    checkComments(index) {
+        if ((index + 1) % 6 === this.state.commentsCount - 2) { 
+            this.readComments(this.state.lastCommentPage + 1)
+            .then(response => {
+                this.setState({
+                    'comments': this.state.comments.concat(response.data.comments),
+                    'commentsCount': this.state.commentsCount + this.state.commentsCount,
+                    'lastCommentPage': this.state.lastCommentPage + 1
+                })
+            })
+        }
+    }
+
+    readComments(page) {
+        return axios('/flowers/' + this.props.match.params.id + '/comments?page=' + page)
     }
 
     openNewComment() {
         this.slider.slickGoTo(0)
         this.setState({
-            'lastCommentPosition': document.getElementsByClassName('slick-current')[0].getAttribute("data-index"), 
+            'lastCommentPosition': document.getElementsByClassName('slick-current')[0].getAttribute("data-index"),
             'newCommentState': true
         })
-        console.log(this.state)
     }
 
     closeNewComment(cancel) {
-        if(cancel) 
-            this.slider.slickGoTo(this.state.lastCommentPosition)
-        this.setState({'newCommentState': false})
+        if (cancel) this.slider.slickGoTo(this.state.lastCommentPosition)
+        this.setState({ 'newCommentState': false })
     }
-
 
     newComment(data) {
         this.closeNewComment(true)
-        console.log(this.props.access)
         axios({
             method: 'post',
             url: '/flowers/' + this.state.flower.id + '/comments/',
             data,
             headers: {
-                authorization: 'Bearer '+ this.props.access 
+                authorization: 'Bearer ' + this.props.access
             },
         }).then((response) => {
-            console.log(response.data)
-            this.setState({'comments': response.data.comments })
+            this.setState({ 'comments': response.data.comments })
         }).then(() => {
             this.closeNewComment()
         })
     }
-    
+
     render() {
         const settings = {
             dots: false, infinite: false, speed: 500,
             slidesToShow: 3, slidesToScroll: 3,
+            afterChange: (index) => { this.checkComments(index) }
         };
 
         return (
             <div className="flower-detail">
-                <SearchHeader/>
-                    {this.state.isLoading ? 
+                <SearchHeader />
+                {this.state.isLoading ?
                     (<div className="main"><h1>loading...</h1></div>) :
                     (<div className="main">
-                        <img src={this.state.flower.images[0].url}/>
+                        <img src={this.state.flower.images[0].url} />
                         <div className="flower-header">
                             <div className="title">
                                 <h2>{this.state.flower.name}</h2>
-                                <img src="https://image.flaticon.com/icons/svg/291/291205.svg" className="star"/>
+                                <img src="https://image.flaticon.com/icons/svg/291/291205.svg" className="star" />
                                 <h2>{this.state.flower.star ? this.state.flower.star : '평가없음'}</h2>
                             </div>
                             <div className="languages">
@@ -100,7 +110,7 @@ class FlowerDetail extends React.Component {
                                 <div>
                                     {this.state.flower.languages.map(value => {
                                         return <Link to={"/search?language=" + value.name} onClick={(event) => event.stopPropagation()}>
-                                        <h5 className="purpose">{value.name}</h5></Link>
+                                            <h5 className="purpose">{value.name}</h5></Link>
                                     })}
                                 </div>
                             </div>
@@ -109,8 +119,8 @@ class FlowerDetail extends React.Component {
                                 <div>
                                     {this.state.flower.purposes.map(value => {
                                         return <Link to={"/search?purpose=" + value.name} onClick={(event) => event.stopPropagation()}>
-                                        <h5 className="purpose">{value.name}</h5>
-                                    </Link>;
+                                            <h5 className="purpose">{value.name}</h5>
+                                        </Link>;
                                     })}
                                 </div>
                             </div>
@@ -126,29 +136,29 @@ class FlowerDetail extends React.Component {
                     </div>)
                 }
 
-                { this.state.isLoading ? 
-                    (<div className="description"/>) : (
-                    <div className="description">
-                        <h3>꽃 설명</h3>
-                        <p>{this.state.flower.description}</p>
-                    </div>)
+                {this.state.isLoading ?
+                    (<div className="description" />) : (
+                        <div className="description">
+                            <h3>꽃 설명</h3>
+                            <p>{this.state.flower.description}</p>
+                        </div>)
                 }
-                
-                { this.state.isLoading ? 
-                    (<div className="comments"/>) : (
-                    <div className="comments">
-                        <h3>댓글</h3>
-                        <div className="slide-wrapper">
-                            <Slider ref={slider => (this.slider = slider)} {...settings} className="comment-slides">
-                                {this.state.newCommentState ? 
-                                    <NewComment newComment={this.newComment} closeNewComment={this.closeNewComment}/> : null}
-                                {this.state.comments.map(value => {
-                                    return <Comment comment={value}/> 
-                                })}
-                            </Slider>
-                        </div>
-                        <button onClick={this.openNewComment}>댓글쓰기</button>
-                    </div>)
+
+                {this.state.isLoading ?
+                    (<div className="comments" />) : (
+                        <div className="comments">
+                            <h3>댓글</h3>
+                            <div className="slide-wrapper">
+                                <Slider ref={slider => (this.slider = slider)} {...settings} className="comment-slides">
+                                    {this.state.newCommentState ?
+                                        <NewComment newComment={this.newComment} closeNewComment={this.closeNewComment} /> : null}
+                                    {this.state.comments.map(value => {
+                                        return <Comment comment={value} />
+                                    })}
+                                </Slider>
+                            </div>
+                            <button onClick={this.openNewComment}>댓글쓰기</button>
+                        </div>)
                 }
             </div>
         );
